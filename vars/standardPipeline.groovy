96992d8f15b7707425c412437be9957e3c169da1
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-09-26 22:17:02 +0100

    OW-834 updating standard pipeline

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -13,30 +13,57 @@ def call(body) {
         }
         
         stages {
-            stage ('Clone') {
+            stage ('Checkout') {
                 steps {
                     checkout scm
                 }
             }        
-            stage("Build") {
+            stage("Compile and Test") {
                 steps {
                     aws {
                         useNexus {
-                            sh './gradlew build -Pprofile=docker  -x test -x integrationTest'
+                            sh './gradlew clean build -Pprofile=docker -x integrationTest'
+                        }
+                    }
+                }
+            }
+            stage("Build Image") {
+                steps {
+                    aws {
+                        useNexus {
+                            sh './gradlew dockerBuildImage -Pprofile=docker -x integrationTest'
+                        }
+                    }
+                }
+            }
+            stage("Push Image") {
+                steps {
+                    aws {
+                        useNexus {
+                            // we push only release branch
+                            switch (env.BRANCH_NAME) {
+                                case "master":
+                                case "develop":
+                                    break
+                                default:
+                                    sh './gradlew dockerPushImage -Pprofile=docker -x integrationTest'
+                                    break
+                            }
                         }
                     }
                 }
             }
             stage("Deploy") {
                 steps {
-                    sh 'ansible-galaxy install -r devops/ansible-deploy/requirements.yml -p devops/ansible-deploy/roles/'
+                    sh "echo to be done"
+                    /*sh 'ansible-galaxy install -r devops/ansible-deploy/requirements.yml -p devops/ansible-deploy/roles/'
                     aws {
                         ansiblePlaybook credentialsId: 'pradeep-cloud-user', 
                             extras: "-e app_name=margin -e app_branch=$BRANCH_NAME", 
                             inventory: 'devops/ansible-deploy/inventory/palo-dev', 
                             playbook: 'devops/ansible-deploy/playbook.yml', 
                             sudoUser: null
-                    }
+                    }*/
                 }
             }   
         }        
