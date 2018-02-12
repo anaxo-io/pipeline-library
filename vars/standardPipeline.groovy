a866fcfaa73e6b53d86f29907d4760dca95080d4
Yong Heng <yongheng@live.com.sg>
2018-02-12 15:37:53 +0800

    add lsmetadata

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -23,6 +23,8 @@ def call(body) {
                 steps {
                     sh 'echo "git_commit=$GIT_COMMIT" >> gradle.properties'
                     sh 'echo "build_number=$BUILD_NUMBER" >> gradle.properties'
+                    sh 'echo "git_branch=$GIT_BRANCH" >> gradle.properties'
+                    sh 'echo "git_branch_no_prefix=${GIT_BRANCH#*/}" >> gradle.properties'
                     sh 'cat gradle.properties'
                 }
             }
