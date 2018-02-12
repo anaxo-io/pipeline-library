a06f32aa88490f63236303929d0c9b22d17937d4
Yong Heng <yongheng@live.com.sg>
2018-02-12 12:39:41 +0800

    write build metadata

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -17,7 +17,14 @@ def call(body) {
                 steps {
                     checkout scm
                 }
-            }        
+            }
+            stage ('Create Build Metadata') {
+                steps {
+                    sh 'echo "git_commit=$GIT_COMMIT" >> gradle.properties'
+                    sh 'echo "build_number=$BUILD_NUMBER" >> gradle.properties'
+                    sh 'cat gradle.properties'
+                }
+            }
             stage("Compile and Test") {
                 steps {
                     aws {
