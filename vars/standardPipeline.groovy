792b3cd3d7370385ceb610fddca76d8557013ee8
Yong Heng <yongheng@live.com.sg>
2018-02-12 12:59:01 +0800

    get deployment to see image rollout with more details

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -62,8 +62,10 @@ def call(body) {
                         useNexus {
                             useKubeConfig {
                                 sh "./gradlew kubernetesNodes"
+                                sh "./gradlew kubernetesGetDeployment"
                                 sh "./gradlew kubernetesDeploy"
                                 sh "./gradlew kubernetesDeployStatus"
+                                sh "./gradlew kubernetesGetDeployment"
                             }
                         }
                     }
