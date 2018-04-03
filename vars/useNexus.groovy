2abc15dfc8eb47f062b4c008062221f5f450e7b2
pbuditi <pbuditi@gmail.com>
2018-04-03 11:57:55 +0800

    Update useNexus.groovy

--- vars/useNexus.groovy
+++ vars/useNexus.groovy
@@ -2,11 +2,11 @@ def call(Closure body) {
   withCredentials([
     [$class: 'StringBinding', credentialsId: 'acuo_nexusUsername', variable: 'ORG_GRADLE_PROJECT_nexusUsername'],
     [$class: 'StringBinding', credentialsId: 'acuo_nexusPassword', variable: 'ORG_GRADLE_PROJECT_nexusPassword'],
-    [$class: 'StringBinding', credentialsId: 'acuo_security_key', variale: 'acuo_security_key_tmp']
+    [$class: 'StringBinding', credentialsId: 'acuo_secret_key', variale: 'acuo_security_key']
   ]) {
     withEnv([
       'ORG_GRADLE_PROJECT_nexusUrl=https://nexus.acuo.com',
-      'acuo_security_key=$acuo_security_key_tmp'
+      'acuo_security_key=$acuo_security_key'
     ]) {
       body()
     }
