d9fd3b3282a2d37f11158c67dbda5986cf6269b5
pbuditi <pbuditi@gmail.com>
2018-04-06 16:47:11 +0800

    Update packagePipeline.groovy

--- vars/packagePipeline.groovy
+++ vars/packagePipeline.groovy
@@ -4,7 +4,7 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-def PB = 'UNKNOW'    
+    
     pipeline {
 
         agent { label 'ubuntu_agent' }
@@ -26,71 +26,7 @@ def PB = 'UNKNOW'
                         }
                     }
                 }
-            }
-            stage("PostBuild") {
-                steps {
-                    build job: '/acuo-common/develop', quietPeriod: 30
-                    PB = sh (build job: 'PostBuild', propagate: false)
-                    result = ${PB}.result
-                    if (result.equals("SUCCESS")){
-                        echo "success"
-                        }
-                        else {
-                            sh "exit 1" // this fails the stage
-                        }
-                    }   
-            }
-            stage("trace") {
-                steps {
-                    node {
-                    build job: '/acuo-trace/develop', quietPeriod: 30
-                    def TB = build job: 'trace', propagate: false
-                    result = TB.result
-                    if (result.equals("SUCCESS")) {
-                        echo "success"
-                    }
-                        else {
-                            sh "exit 2"
-                        }
-                    }
-                }
-            }
-            stage("persist") {
-                steps {
-                    node {
-                    build job: '/acuo-persist/develop', quietPeriod: 30
-                    def PT = build job: 'persist', quietPeriod: 30
-                    result = PT.result
-                    if (result.equals("SUCCESS")) {
-                        echo "success"
-                    }
-                        else {
-                            sh "exit 3"
-                        }
-                    }
-                }
-            }
-            stage("all") {
-                steps{
-                        parallel (
-                            a: {
-                                build job: '/acuo-auth/develop', quietPeriod: 30
-                                },
-                            b: {
-                                build job: '/acuo-agrement/develop', quietPeriod: 30
-                                },
-                            c: {
-                                build job: '/acuo-margin/develop', quietPeriod: 30
-                                },
-                            d: {
-                                build job: '/acuo-valuation/develop', quietPeriod: 30
-                                },
-                            e: {
-                                build job: '/acuo-collateral/develop',quietPeriod: 30
-                            }
-                            )
-                    }
-                }       
+            }           
         }        
     }
 }
