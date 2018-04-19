33ae04e6b50c5a7dd8a9342cef4bd143f03df21a
vidarbha <vidarbha009@gmail.com>
2018-04-19 18:21:07 +0800

    Update createBuildMetadata.groovy

--- vars/createBuildMetadata.groovy
+++ vars/createBuildMetadata.groovy
@@ -3,7 +3,7 @@ def call() {
     sh 'echo "build_number=$BUILD_NUMBER" >> gradle.properties'
     sh 'echo "git_branch=$GIT_BRANCH" >> gradle.properties'
     sh 'echo "git_branch_no_prefix=${GIT_BRANCH#*/}" >> gradle.properties'
-    sh 'echo "nexusUrl=${nexusUrl}" >> gradle.properties'
-    sh 'echo "nexusUsername=${nexusUsername}" >> gradle.properties'
-    sh 'echo "nexusPassword=${nexusPassword}" >> gradle.properties'
+    sh 'echo "nexusUrl=https://nexus.acuo.com" >> gradle.properties'
+    sh 'echo "nexusUsername=deployer" >> gradle.properties'
+    sh 'echo "nexusPassword=VBF4ROzCO1P7cUVnE05t" >> gradle.properties'
 }
