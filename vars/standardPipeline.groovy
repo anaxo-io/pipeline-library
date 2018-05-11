6ec5cc5517f75c889eab6e296ed880a3963af32e
Hicham Medkouri <hicham.medkouri@gmail.com>
2018-05-11 10:12:33 +0100

    OW-812 traying to exclude other branches than develop

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -5,100 +5,101 @@ def call(body) {
     body.delegate = config
     body()
     
-    pipeline {
+    if (env.BRANCH_NAME == "develop") {
+        pipeline {
 
-        agent { label 'ubuntu_agent' }
-        triggers {
-            pollSCM("")
-        }
-        
-        stages {
-            stage ('Checkout') {
-                steps {
-                    deleteDir()
-                    checkout scm
-                }
+            agent { label 'ubuntu_agent' }
+            triggers {
+                pollSCM("")
             }
-            stage ('Create Build Metadata') {
-                steps {
-                    createBuildMetadata()
-                }
-            }
-            stage ('Print gradle.properties') {
-                steps {
-                    sh 'cat gradle.properties'
-                }
-            }
-            stage("Compile and Test") {
-                steps {
-                    aws {
-                        useNexus {
-                            sh './gradlew clean build -Pprofile=docker -x integrationTest'
-                        }
+            
+            stages {
+                stage ('Checkout') {
+                    steps {
+                        deleteDir()
+                        checkout scm
                     }
                 }
-            }
-            stage("Build Image") {
-                steps {
-                    aws {
-                        useNexus {
-                            sh './gradlew dockerBuildImage -Pprofile=docker -x integrationTest'
-                        }
+                stage ('Create Build Metadata') {
+                    steps {
+                        createBuildMetadata()
                     }
                 }
-            }
-            stage("Push Image") {
-                steps {
-                    aws {
-                        useNexus {
-                            sh './gradlew dockerPushImage -Pprofile=docker -x integrationTest'
-                        }
+                stage ('Print gradle.properties') {
+                    steps {
+                        sh 'cat gradle.properties'
                     }
                 }
-            }
-            /*stage("Kubernetes Deploy 'acuo'") {
-                when {
-                    expression {
-                        return env.BRANCH_NAME == "develop"
-                    }
-                }
-                steps {
-                    aws {
-                        useNexus {
-                            withEnv(['K8_NAMESPACE=acuo']) {
-                                useKubeConfig {
-                                    kubeDeploy()
-                                }
+                stage("Compile and Test") {
+                    steps {
+                        aws {
+                            useNexus {
+                                sh './gradlew clean build -Pprofile=docker -x integrationTest'
                             }
                         }
                     }
                 }
-            }*/ 
-            stage("Kubernetes Deploy 'qa'") {
-                when {
-                    expression {
-                        return env.BRANCH_NAME == "develop"
-                    }
-                }
-                steps {
-                    aws {
-                        useNexus {
-                            withEnv(['K8_NAMESPACE=qa']) {
-                                useKubeConfig {
-                                    kubeDeploy()
-                                }
+                stage("Build Image") {
+                    steps {
+                        aws {
+                            useNexus {
+                                sh './gradlew dockerBuildImage -Pprofile=docker -x integrationTest'
                             }
                         }
                     }
                 }
-            }   
-        }        
-        
-        post {
-            always {
-                junit allowEmptyResults: true, testResults: '${config.projectName}/build/test-results/test/*.xml'
+                stage("Push Image") {
+                    steps {
+                        aws {
+                            useNexus {
+                                sh './gradlew dockerPushImage -Pprofile=docker -x integrationTest'
+                            }
+                        }
+                    }
+                }
+                /*stage("Kubernetes Deploy 'acuo'") {
+                    when {
+                        expression {
+                            return env.BRANCH_NAME == "develop"
+                        }
+                    }
+                    steps {
+                        aws {
+                            useNexus {
+                                withEnv(['K8_NAMESPACE=acuo']) {
+                                    useKubeConfig {
+                                        kubeDeploy()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }*/ 
+                stage("Kubernetes Deploy 'qa'") {
+                    when {
+                        expression {
+                            return env.BRANCH_NAME == "develop"
+                        }
+                    }
+                    steps {
+                        aws {
+                            useNexus {
+                                withEnv(['K8_NAMESPACE=qa']) {
+                                    useKubeConfig {
+                                        kubeDeploy()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }   
+            }        
+            
+            post {
+                always {
+                    junit allowEmptyResults: true, testResults: '${config.projectName}/build/test-results/test/*.xml'
+                }
             }
         }
-
     }
 }
